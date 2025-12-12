6cc4e4247a0d33ca2b55828ffaa10188b27f319b
Aravind162003 <aravindsh03@gmail.com>
2025-12-12 20:10:00 +0530

    initial commit

---
+++ src/main/java/com/aravind/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.aravind.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello Aravind! Your Spring Boot app is running!";
+    }
+}
